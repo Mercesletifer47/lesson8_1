c06489ddee679809ffeec5174db4fec7e0e5c145
Mercesletifer47 <art-matv98@yandex.ru>
2021-07-15 16:23:48 +0300

    Dev

--- src/ru/geekbrains/HomeWorkApp_lesson8_1/TicTacToe.java
+++ src/ru/geekbrains/HomeWorkApp_lesson8_1/TicTacToe.java
@@ -3,6 +3,9 @@ package ru.geekbrains.HomeWorkApp_lesson8_1;
 import javax.swing.*;
 import java.awt.*;
 
+//1. Добработать рисование крестиков и ноликов в игровом поле.
+//        2* . Показать победителя.
+//        3***. Нарисовать выиграшную линию
 public class TicTacToe {
     public TicTacToe() {
     }
